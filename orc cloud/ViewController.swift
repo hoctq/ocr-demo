183656e5149974254a063a71327745baff52b5ba
Hoc Trinh Quang <hoctq@tmh-techlab.vn>
2019-09-09 11:38:11 +0700

    Initial Commit

---
+++ orc cloud/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  orc cloud
+//
+//  Created by Trinh Quang Hoc on 9/9/19.
+//  Copyright © 2019 Trinh Quang Hoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
